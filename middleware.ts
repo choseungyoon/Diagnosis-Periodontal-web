143ceb7165e57de6f930a20652dac949494c551a
choseungyoon <cho31250@gmail.com>
2024-11-27 21:50:00 +0900

    Add middleware

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const allowedIps = process.env.ALLOWED_IPS?.split(',') || [];
+  const clientIp = req.ip || req.headers.get('x-forwarded-for')?.split(',')[0] || '';
+
+  console.log(clientIp)
+  // IP 인증 로직
+  if (!allowedIps.includes(clientIp)) {
+    return new NextResponse(
+      JSON.stringify({ message: 'Access denied' }),
+      { status: 403, headers: { 'Content-Type': 'application/json' } }
+    );
+  }
+
+  // 인증 성공 시 요청 계속 진행
+  return NextResponse.next();
+}
